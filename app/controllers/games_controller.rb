76c20dc2184fd099684ea4cc6c8712c46b0f0b0b
Clement Bouakkaz <clementbouakkaz@gmail.com>
2021-06-03 17:15:57 +0200

    font changes

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
 skip_before_action :authenticate_user!, only: [ :index ]
 
   def index
-    @games = policy_scope(Game)
+    @games = policy_scope(Game).limit(4)
   end
 
   def show
